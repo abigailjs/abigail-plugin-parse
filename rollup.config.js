fc17144fd8926a81308e36a8bbc656500d86cf65
59naga <i59naga@icloud.com>
2016-04-08 15:09:54 +0900

    chore(build): use rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'src/index.js',
+  dest: 'lib/index.js',
+  sourceMap: true,
+  format: 'umd',
+  plugins: [
+    nodeResolve({
+      skip: ['abigail-plugin'],
+    }),
+    commonjs(),
+    babel(),
+    uglify(),
+  ],
+};
